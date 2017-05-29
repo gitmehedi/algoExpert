9edf3b1e343454bffba6ed77b9eb3a423cc5b4ab
gitmehedi <md.mehedi.info@gmail.com>
2017-05-29 09:22:33 +0600

    [ADD-MEHEDI] Add homework for counting sort and dijkstra algorithm

---
+++ homework/dijkstra_algorithm/mehedi_python.py
@@ -0,0 +1,27 @@
+"""
+Description about Dijkstra algorithm
+
+Time Complexity
+================
+
+Best Case: Theta()
+Avg. Case: Omega()
+Worst Case: BigOh()
+
+"""
+
+class DijkstraAlgorithm(object):
+
+    def __init__(self, lists):
+        self.arr = lists
+
+    def sorting(self):
+        return True
+
+    def show_list(self):
+        print ("Dijkstra Algorithm")
+
+
+lists = ''
+dijkstra_sort = DijkstraAlgorithm(lists)
+
