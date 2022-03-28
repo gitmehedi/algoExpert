89ef6231cc529a5445eac0d1281f72f61f8f0398
gitmehedi <git.mehedi@gmail.com>
2022-03-28 12:19:04 +0600

    [ADD] Add LC algorithm no 500 and 1337

---
+++ leetcode/1337_The_K_Weakest_Rows_in_a_Matrix.py
@@ -0,0 +1,95 @@
+"""
+1337. The K Weakest Rows in a Matrix
+
+You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
+
+A row i is weaker than a row j if one of the following is true:
+
+The number of soldiers in row i is less than the number of soldiers in row j.
+Both rows have the same number of soldiers and i < j.
+Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+
+
+
+Example 1:
+
+Input: mat =
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]],
+k = 3
+Output: [2,0,3]
+Explanation:
+The number of soldiers in each row is:
+- Row 0: 2
+- Row 1: 4
+- Row 2: 1
+- Row 3: 2
+- Row 4: 5
+The rows ordered from weakest to strongest are [2,0,3,1,4].
+Example 2:
+
+Input: mat =
+[[1,0,0,0],
+ [1,1,1,1],
+ [1,0,0,0],
+ [1,0,0,0]],
+k = 2
+Output: [0,2]
+Explanation:
+The number of soldiers in each row is:
+- Row 0: 1
+- Row 1: 4
+- Row 2: 1
+- Row 3: 1
+The rows ordered from weakest to strongest are [0,2,3,1].
+
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+2 <= n, m <= 100
+1 <= k <= m
+matrix[i][j] is either 0 or 1.
+"""
+import resource
+import time
+from typing import List
+
+time_start = time.perf_counter()
+
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        mat_arr = [sum(list(filter(lambda x: x == 1, val))) for val in mat]
+        mat_sort = [(ind, val) for ind, val in enumerate(mat_arr)]
+        weekest = [val[0] for val in sorted(mat_sort, key=lambda x: x[1])]
+
+        return weekest[:k]
+
+
+mat = [[1, 1, 0, 0, 0],
+       [1, 1, 1, 1, 0],
+       [1, 0, 0, 0, 0],
+       [1, 1, 0, 0, 0],
+       [1, 1, 1, 1, 1]]
+k = 3
+
+mat = [[1, 0, 0, 0],
+       [1, 1, 1, 1],
+       [1, 0, 0, 0],
+       [1, 0, 0, 0]]
+k = 2
+obj = Solution()
+result = obj.kWeakestRows(mat, k)
+print(result)
+
+"""
+Time and Memory Calculation
+"""
+time_elapsed = (time.perf_counter() - time_start)
+memMb = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024.0 / 1024
+print("Time  : %5.5f secs \nMemory: %5.5f KByte" % (time_elapsed, memMb))
---
+++ leetcode/500_Keyboard_Row.py
@@ -0,0 +1,59 @@
+"""
+500. Keyboard Row
+Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American keyboard like the image below.
+
+In the American keyboard:
+
+the first row consists of the characters "qwertyuiop",
+the second row consists of the characters "asdfghjkl", and
+the third row consists of the characters "zxcvbnm".
+
+Example 1:
+
+Input: words = ["Hello","Alaska","Dad","Peace"]
+Output: ["Alaska","Dad"]
+Example 2:
+
+Input: words = ["omk"]
+Output: []
+Example 3:
+
+Input: words = ["adsdf","sfd"]
+Output: ["adsdf","sfd"]
+
+
+Constraints:
+
+1 <= words.length <= 20
+1 <= words[i].length <= 100
+words[i] consists of English letters (both lowercase and uppercase).
+"""
+import resource
+import time
+from typing import List
+
+time_start = time.perf_counter()
+
+
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        fr = "qwertyuiop"
+        sr = "asdfghjkl"
+        tr = "zxcvbnm"
+        for val in words:
+            pass
+
+
+words = ["Hello", "Alaska", "Dad", "Peace"]
+# words = ["omk"]
+# words = ["adsdf", "sfd"]
+obj = Solution()
+result = obj.findWords(words)
+print(result)
+
+"""
+Time and Memory Calculation
+"""
+time_elapsed = (time.perf_counter() - time_start)
+memMb = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024.0 / 1024
+print("Time  : %5.5f secs \nMemory: %5.5f KByte" % (time_elapsed, memMb))
