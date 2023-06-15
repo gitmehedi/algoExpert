0c439bee9a15c162f82ea9d2e1b404bfed1c0fbc
gitmehedi <git.mehedi@gmail.com>
2023-06-15 09:10:14 +0600

    [ADD] Add new algorithm for runlengthencoding

--- algoexpert/Strings/run_length_encoding.py
+++ algoexpert/Strings/run_length_encoding.py
@@ -2,3 +2,17 @@
 URL: https://www.algoexpert.io/questions/run-length-encoding
 
 """
+
+def runLengthEncoding(string):
+    encodeArr, count, i = [], 0, 0
+
+    while i < len(string) - 1:
+        if string[i] == string[i + 1] and count < 9:
+            count += 1
+        else:
+            encodeArr.append(str(count) + string[i])
+            count = 1
+        i = i + 1
+    encodeArr.append(str(count) + string[i])
+
+    return "".join(encodeArr)
--- algoexpert/algoexpert.py
+++ algoexpert/algoexpert.py
@@ -1,7 +1,37 @@
+def runLengthEncoding(string):
+    encodeArr, count, i = [], 0, 0
+
+    while i < len(string) - 1:
+        if string[i] == string[i + 1] and count < 9:
+            count += 1
+        else:
+            encodeArr.append(str(count) + string[i])
+            count = 1
+        i = i + 1
+    encodeArr.append(str(count) + string[i])
+
+    return "".join(encodeArr)
+
+def runLengthEncoding(string):
+    # Write your code here.
+    encodeArr, count = [], 1
+    for i in range(len(string) - 1):
+        if string[i] == string[i + 1] and count < 9:
+            count += 1
+        else:
+            encodeArr.append(str(count) + string[i])
+            count = 1
+
+    encodeArr.append(str(count) + string[-1])
+
+    return "".join(encodeArr)
 
 
+string = [
+    'AAAAAAAAAAAAABBCCCCDD',
+    'aA',
+]
 
-sample = 'zab'
-response1 = solution(sample)
-print(response1)
-print(response2)
+for st in string:
+    res = runLengthEncoding(st)
+    print(res)
