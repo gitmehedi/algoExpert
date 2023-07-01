542b7fe4be1aa710c386a7d7ec7af3e812fc6dcf
gitmehedi <git.mehedi@gmail.com>
2023-07-01 18:18:19 +0600

    [ADD] Add linkedlist merge features

--- algoexpert/experiment.py
+++ algoexpert/experiment.py
@@ -1,72 +1,48 @@
 class Node:
-    def __init__(self, data=None):
+    def __init__(self, data):
         self.data = data
         self.next = None
 
 
-class LinkedList:
+class ListNode:
     def __init__(self):
-        self.root = None
+        self.head = None
 
     def insert(self, data):
         new_node = Node(data)
-        if self.root:
-            if self.root.data > data:
-                new_node.next = self.root
-                self.root = new_node
-            else:
-                current = self.root
-                while current.next:
-                    if current.next.data > data:
-                        break
-                    current = current.next
-                new_node.next = current.next
-                current.next = new_node
+        if self.head:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
         else:
-            self.root = new_node
+            self.head = new_node
 
-    def print_list(self):
+    def mergeList(self):
         vals = []
-        current = self.root
+        sum = 0
+        current = self.head
+        while current:
+            vals.append(current.data)
+            sum = sum + current.data
+            current = current.next
+
+        print(sum)
+        valint = int("".join([str(i) for i in vals]), 2)
+        print(valint)
+
+    def print_tree(self):
+        vals = []
+        current = self.head
         while current:
             vals.append(current.data)
             current = current.next
-
         print(vals)
 
 
-class Solution:
-    def mergeTwoList(self, list1, list2):
-        head = Node(0)
-        current = head
-
-        cur1 = list1.root
-        cur2 = list2.root
-        while cur1.next and cur2.next:
-            if cur1.data > cur2.data:
-                current.next = cur1.next
-                cur1 = cur1.next
-            else:
-                current.next = cur2.data
-                cur2 = cur2.next
-            current = current.next
-
-        current.next = cur1 or cur2
-        return head.next
-
-
-build = LinkedList()
-build2 = LinkedList()
-list1 = [1, 2, 4]
-list2 = [1, 3, 4]
-for val in list1:
+build = ListNode()
+lsn = [1, 0, 1, 0, 1]
+for val in lsn:
     build.insert(val)
-
-for val2 in list2:
-    build2.insert(val2)
-
-build.print_list()
-build2.print_list()
-
-solution = Solution()
-solution.mergeTwoList(build, build2)
+build.print_tree()
+build.mergeList()
