b6e06927b2bbd8585597342894bbfc20f3d09b2c
gitmehedi <git.mehedi@gmail.com>
2023-07-01 20:53:56 +0600

    [MOD] Change on algorithm

--- algoexpert/experiment.py
+++ algoexpert/experiment.py
@@ -18,18 +18,40 @@ class ListNode:
         else:
             self.head = new_node
 
-    def mergeList(self):
-        vals = []
-        sum = 0
+    def reverseList(self):
+        new_list = None
         current = self.head
         while current:
-            vals.append(current.data)
-            sum = sum + current.data
+            next_node = current.next
+            current.next = new_list
+            new_list = current
+            current = next_node
+        self.head = new_list
+
+    def removeDuplicates(self):
+        current = self.head
+        prev = None
+        lst = []
+        while current:
+            if current.data not in lst:
+                lst.append(current.data)
+                prev = current
+            else:
+                prev.next = current.next
             current = current.next
 
-        print(sum)
-        valint = int("".join([str(i) for i in vals]), 2)
-        print(valint)
+    def removeDuplicate(self):
+        if self.head is None:
+            return False
+
+        current = self.head
+        while current.next:
+            if current.data == current.next.data:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return self.head
 
     def print_tree(self):
         vals = []
@@ -41,8 +63,11 @@ class ListNode:
 
 
 build = ListNode()
-lsn = [1, 0, 1, 0, 1]
+lsn = [13, 12, 11, 3, 5, 1, 2, 7, 6, 8]
+lsn = [1, 1, 2]
 for val in lsn:
     build.insert(val)
 build.print_tree()
-build.mergeList()
+build.removeDuplicates()
+# build.removeDuplicate()
+build.print_tree()
