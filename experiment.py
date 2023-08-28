d9fe700e4dd77288472c44c0743b5feaaa8f5966
gitmehedi <git.mehedi@gmail.com>
2023-08-28 10:25:53 +0600

    [ADD] Add all the values

--- experiment.py
+++ experiment.py
@@ -1,17 +1,22 @@
 """
 
 """
-
-citations = [0, 3, 6, 1, 5]
-
-
-def hIndex(citations):
-    citations.sort()
-    length = len(citations)
-    for i in range(length):
-        if citations[i] >= length - i:
-            return length - i
-    return 0
+ratings = [1, 0, 2]
+ratings = [1, 2, 2]
+ratings = [1, 3, 2, 2, 1]
 
 
-print(hIndex(citations))
+def candy(ratings):
+    rlen = len(ratings)
+    candy = [1] * rlen
+    for i in range(1, rlen):
+        if ratings[i] > ratings[i - 1]:
+            candy[i] = candy[i - 1] + 1
+
+    for i in range(rlen - 2, -1, -1):
+        if ratings[i] > ratings[i + 1]:
+            candy[i] = max(candy[i], candy[i + 1] + 1)
+    return sum(candy)
+
+
+print(candy(ratings))
