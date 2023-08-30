2eb6badfedb41f3ed70c48b177f18e44323fd138
gitmehedi <git.mehedi@gmail.com>
2023-08-30 10:51:56 +0600

    [ADD] Add various algorithm

--- experiment.py
+++ experiment.py
@@ -1,4 +1,29 @@
 """
 
 """
+# nums = [-1, 0, 1, 2, -1, -4]
+nums = [-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]
 
+
+def threeSum(nums):
+    nums = sorted(nums)
+    result = set()
+
+    for num in range(len(nums)):
+        left = num + 1
+        right = len(nums) - 1
+        while left < right:
+            cursum = nums[num] + nums[left] + nums[right]
+            if cursum == 0:
+                result.add((nums[num], nums[left], nums[right]))
+                left += 1
+                right -= 1
+            elif cursum > 0:
+                right -= 1
+            elif cursum < 0:
+                left += 1
+
+    return result
+
+
+print(threeSum(nums))
---
+++ interview_150/two_pointers/392. Is Subsequence.py
@@ -0,0 +1,41 @@
+"""
+Problem: https://leetcode.com/problems/is-subsequence/description/?envType=study-plan-v2&envId=top-interview-150
+392. Is Subsequence
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+
+"""
+
+s = "abc"
+t = "ahbgdc"
+
+
+def isSubsequence(s, t):
+    first, sec = 0, 0
+    while first < len(s) and sec < len(t):
+        if s[first] == t[sec]:
+            first += 1
+        sec += 1
+    return first == len(s)
+
+
+print(isSubsequence(s, t))
